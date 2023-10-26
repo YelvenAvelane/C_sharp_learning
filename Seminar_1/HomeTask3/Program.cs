1b61d2ad8ea4d1ba78fdd37c4fd99d16b8342207
Polina <polinko-007@mail.ru>
2023-10-26 16:54:46 +0500

    fxvbx

---
+++ Seminar_1/HomeTask3/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт, является ли оно чётным или нет
+
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+
+if (num%2 == 0)
+{
+    Console.WriteLine("Четное");
+}
+else
+{
+    Console.WriteLine("Нечетное");
+}
