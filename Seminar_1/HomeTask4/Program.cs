b1744ee2cee853554aaa1587131732dbbc64a041
Polina <polinko-007@mail.ru>
2023-10-26 17:14:45 +0500

    sdvsdv

---
+++ Seminar_1/HomeTask4/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая на вход получает число N, а на выходе показывает все чётные числа от 1 до N
+
+Console.Write("Введите число: ");
+string strNum = Console.ReadLine();
+int num = int.Parse(strNum);
+int num0 = 1;
+
+while (num0 <= num)
+{
+    if (num0%2 == 0)
+    {
+        Console.WriteLine(num0);
+    }
+num0 ++;
+}
