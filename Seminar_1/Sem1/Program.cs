b41fd503fe07dd4f69f8c2888c1b607bdbb6c8d4
Polina <polinko-007@mail.ru>
2023-10-26 14:32:51 +0500

    sdg

--- Seminar_1/Sem1/Program.cs
+++ Seminar_1/Sem1/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает число и выдаёт его квадрат (число, умноженное само на себя)
+// Например: 4 -> 16, -3 -> 9 и т.д. 
+
+Console.Write("Введите число: ");
+string strNum = Console.ReadLine(); // Мы получили введенные данные и записали в нашу переменную типа string
+int num = int.Parse(strNum);  //  Parse - приобразование строки в число (  аналог Convert.ToInt32() )
+int sqr = num * num;
+Console.WriteLine(sqr);
