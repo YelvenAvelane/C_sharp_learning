e45b7246bfba12666d94c41590c6781308879d34
Polina <polinko-007@mail.ru>
2023-10-26 16:26:49 +0500

    sdfsdf

---
+++ Seminar_1/Task5/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая на вход принимает 3-х значное число, а на выходе показывает последнюю цифру этого числа
+// Например 
+// 456 -> 6
+
+Console.Write("Введите число: ");
+string strNum = Console.ReadLine();
+int num = int.Parse(strNum);
+// Аналогом предыдущих двух строчек будет строчка
+//int num = int.Parse(Console.ReadLine());"
+
+if (num >= 100 && num <= 999)
+{
+    Console.WriteLine(num%10);
+}
+else
+{
+    Console.WriteLine("Некорректное число");
+}
