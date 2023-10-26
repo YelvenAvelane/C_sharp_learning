5205c0d4a1b593f6ec14cfee042d7ecd02f85b96
Polina <polinko-007@mail.ru>
2023-10-26 15:00:07 +0500

    sdgdf

--- Seminar_1/Sem2/Program.cs
+++ Seminar_1/Sem2/Program.cs
@@ -5,16 +5,20 @@
 
 Console.Write("Введите первое число: ");
 string firstNum = Console.ReadLine();
-int num1 = int.Parse(firstNum); 
+int num1 = int.Parse(firstNum);
+
 Console.Write("Введите второе число: ");
 string secondNum = Console.ReadLine();
+
 int num2 = int.Parse(secondNum);
-int sqr = num1 * num1;
-if (sqr == num2)
+
+int sqr = num2 * num2;
+
+if (sqr == num1)
 {
-    Console.WriteLine(да);
+    Console.WriteLine("да");
 }
 else
 {
-    Console.WriteLine(нет);
+    Console.WriteLine("нет");
 }
