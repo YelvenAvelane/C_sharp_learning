3bb9f061f2d3f74ae7e3fa7175b0b1f43f6f3cdc
Polina <polinko-007@mail.ru>
2023-10-29 13:57:37 +0500

    ывывывм

--- Seminar_2/Task1/Program.cs
+++ Seminar_2/Task1/Program.cs
@@ -1,3 +1,18 @@
 ﻿// Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа
 
-int array = 
+int num = new Random().Next (10,100);
+Console.WriteLine(num);
+
+int num1 = num / 10; // 94/10 = 9
+int num2 = num % 10; // 94%10 = 4
+
+if (num1 > num2)
+{
+    Console.WriteLine(num1); // Ещё рабочий вариант   Console.WriteLine("max = " + num1)
+}
+else
+{
+    Console.WriteLine(num2); // Ещё рабочий вариант   Console.WriteLine("max = " + num2)
+}
+
+// Ещё один аналог       Console.WriteLine($"max = {num1}"); // это интерполяция строк
