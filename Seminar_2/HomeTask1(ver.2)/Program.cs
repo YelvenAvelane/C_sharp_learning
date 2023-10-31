2e7a1f109d2e4c2b856662174a96039322a9d822
Polina <polinko-007@mail.ru>
2023-10-31 11:27:00 +0500

    вмвчям

---
+++ Seminar_2/HomeTask1(ver.2)/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет
+
+Console.Write("Вввдите число: ");
+string strNum = Console.ReadLine();
+int num = int.Parse(strNum);
+
+if (num<100)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+Console.WriteLine(strNum[2]);
+
+// Так себе способ наверное. В теории можно оставить 3 строчки, но при вводе числа <100 или букв, не будет выводиться ошибка о некорректности ввода
+
+// Console.Write("Вввдите число: ");
+// string strNum = Console.ReadLine();
+// Console.WriteLine(strNum[2]);
