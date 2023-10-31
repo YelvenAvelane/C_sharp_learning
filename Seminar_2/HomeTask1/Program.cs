6aab43f09e9ae7b66fd504d09fea55b293205d50
Polina <polinko-007@mail.ru>
2023-10-31 11:09:55 +0500

    ваиваи

---
+++ Seminar_2/HomeTask1/Program.cs
@@ -0,0 +1,52 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет
+
+Console.Write("Вввдите число: ");
+string strNum = Console.ReadLine();
+int num = int.Parse(strNum);
+
+if (num<100)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+else
+{
+    if (num>99 && num<1000)
+    {
+        int result = num % 10;
+        Console.WriteLine(result);
+    }
+    else if (num>990 && num<10000)
+    {
+        int result = (num % 100) / 10;
+        Console.WriteLine(result);
+    }
+    else if (num>9900 && num<100000)
+    {
+        int result = (num % 1000) / 100;
+        Console.WriteLine(result);
+    }
+    else if (num>99000 && num<1000000)
+    {
+        int result = (num % 10000) / 1000;
+        Console.WriteLine(result);
+    }
+    else if (num>990000 && num<10000000)
+    {
+        int result = (num % 100000) / 10000;
+        Console.WriteLine(result);
+    }
+    else if (num>9900000 && num<100000000)
+    {
+        int result = (num % 1000000) / 100000;
+        Console.WriteLine(result);
+    }
+    else if (num>99000000 && num<1000000000)
+    {
+        int result = (num % 10000000) / 1000000;
+        Console.WriteLine(result);
+    }
+    else
+    {
+        Console.WriteLine("Очень больше число, сорян");
+    }
+}
---
+++ Seminar_2/HomeTask2/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и првоеряет, является ли этот день выходным
+
+Console.Write("Введите число: ");
+string strNum = Console.ReadLine();
+int dayNum = int.Parse(strNum); // Эта строка не обязательна. Вместо неё можно числа в ветвлениях записать в кавычки. Например: if (dayNum == "1"). Тот же результат.
+
+int [] weekdays = {1,2,3,4,5};
+int [] weekend = {6,7};
+
+if (weekdays.Contains(dayNum))
+{
+    Console.WriteLine("Будний день");
+}
+else if (weekend.Contains(dayNum))
+{
+    Console.WriteLine("Выходной день");
+}
+else
+{
+    Console.WriteLine("Некорректное число");
+}
