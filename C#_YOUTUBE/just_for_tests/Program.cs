65d1a1d8509025c7ef2a29a38486844e2f4759a7
Polina <polinko-007@mail.ru>
2023-11-03 14:53:01 +0500

    прол

--- C#_YOUTUBE/just_for_tests/Program.cs
+++ C#_YOUTUBE/just_for_tests/Program.cs
@@ -1,14 +1,1 @@
-﻿Console.Write("Введите число: ");
-string strNum = Console.ReadLine(); 
-double num = double.Parse(strNum);
-
-if (num%2 == 0)
-{
-Console.WriteLine("Чётное");
-}
-else
-{
-    Console.WriteLine("НЕчётное");
-}
-
-
+﻿
