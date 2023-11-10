9e34e66b295c2370a729e9d77dde92747271ec61
Polina <polinko-007@mail.ru>
2023-11-10 22:12:44 +0500

    dfhgjhk

--- C#_YOUTUBE/just_for_tests/Program.cs
+++ C#_YOUTUBE/just_for_tests/Program.cs
@@ -1,1 +1,59 @@
-﻿
+﻿Console.Write("Введите высоту треугольника: ");
+int height = int.Parse(Console.ReadLine());
+
+// Треугольник 1
+
+for (int i = 0; i <= height; i++)
+{
+    for (int j = 0; j < i; j++)
+    {
+        Console.Write("*");
+    } 
+    Console.WriteLine();
+}
+
+Console.WriteLine();
+
+// Треугольник 2
+
+for (int i = 0; i < height; i++)
+{
+    for (int j = height; j > i; j--)
+    {
+        Console.Write("*");
+    } 
+    Console.WriteLine();
+}
+
+Console.WriteLine();
+
+// Треугольник 3
+for (int i = 0; i <= height; i++)
+{
+    for (int j = height; j > i; j--)
+    {
+        Console.Write(" ");
+    }
+    for (int j = 0; j < i; j++)
+    {
+        Console.Write("*");
+    } 
+    Console.WriteLine();
+}
+
+Console.WriteLine();
+
+// Треугольник 4
+
+for (int i = 0; i < height; i++)
+{
+    for (int j = 0; j < i; j++)
+    {
+        Console.Write(" ");
+    } 
+    for (int j = height; j > i; j--)
+    {
+        Console.Write("*");
+    } 
+    Console.WriteLine();
+}
