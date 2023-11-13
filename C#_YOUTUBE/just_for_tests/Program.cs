617fed075c4d361f0951905d56ed38cef523a1b7
Polina <polinko-007@mail.ru>
2023-11-13 11:32:37 +0500

    dfgdfg

--- C#_YOUTUBE/just_for_tests/Program.cs
+++ C#_YOUTUBE/just_for_tests/Program.cs
@@ -1,1 +1,30 @@
-﻿
+﻿// ЗАДАЧА 1
+// int [] array = new int [5];
+// array[0] = int.Parse(Console.ReadLine());
+// array[1] = int.Parse(Console.ReadLine());
+// array[2] = int.Parse(Console.ReadLine());
+// array[3] = int.Parse(Console.ReadLine());
+// array[4] = int.Parse(Console.ReadLine());
+
+// Console.Write("{0}", string.Join("\n", array));
+
+// ЗАДАЧА 2
+// int[] array = {3,6,2,88,31};
+
+// for (int i = 4; i >= 0 ; i--)
+// {
+//     Console.WriteLine(array[i]);
+// }
+
+// ЗАДАЧА 3
+int[] array = {5,6,2,89,31};
+int sum = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] % 2 == 0)
+    {
+        sum=sum+i;
+    }
+}
+Console.WriteLine(sum);
