e097c3fc62d963437809a641db9cadb600afc346
Polina <polinko-007@mail.ru>
2023-11-16 10:45:18 +0500

    fdg

--- C#_YOUTUBE/just_for_tests/Program.cs
+++ C#_YOUTUBE/just_for_tests/Program.cs
@@ -1,30 +1,4 @@
-﻿// ЗАДАЧА 1
-// int [] array = new int [5];
-// array[0] = int.Parse(Console.ReadLine());
-// array[1] = int.Parse(Console.ReadLine());
-// array[2] = int.Parse(Console.ReadLine());
-// array[3] = int.Parse(Console.ReadLine());
-// array[4] = int.Parse(Console.ReadLine());
-
-// Console.Write("{0}", string.Join("\n", array));
-
-// ЗАДАЧА 2
-// int[] array = {3,6,2,88,31};
-
-// for (int i = 4; i >= 0 ; i--)
-// {
-//     Console.WriteLine(array[i]);
-// }
-
-// ЗАДАЧА 3
-int[] array = {5,6,2,89,31};
-int sum = 0;
-
-for (int i = 0; i < array.Length; i++)
-{
-    if (array[i] % 2 == 0)
-    {
-        sum=sum+i;
-    }
-}
-Console.WriteLine(sum);
+﻿int[] array = { 3, 116, 3, 2, 45, 2, 89 };
+int[] result = array.Where(i => i < 70).ToArray();
+Console.WriteLine(result);
+Console.ReadLine();
